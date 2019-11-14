6f7bf743fcfb701a693bf48c1c07e5e8dc1a3cc9
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-11-14 23:23:43 +0100

    Add react lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { IndexRoute } from 'react-router';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 //routes
-import Home from './components/pages/Home/HomePage';
-import Posts from './components/pages/Posts/PostsPage';
-import Contact from './components/pages/Contact/ContactPage';
-import NotFound from './components/pages/NotFound/NotFoundPage';
-import SinglePost from './components/pages/SinglePost/SinglePost';
-import NewPost from './components/pages/NewPost/NewPost';
+const Home = lazy(() => import('./components/pages/Home/HomePage'));
+const Posts = lazy(() => import('./components/pages/Posts/PostsPage'));
+const Contact = lazy(() => import('./components/pages/Contact/ContactPage'));
+const NotFound = lazy(() => import('./components/pages/NotFound/NotFoundPage'));
+const SinglePost = lazy(() => import('./components/pages/SinglePost/SinglePost'));
+const NewPost = lazy(() => import('./components/pages/NewPost/NewPost'));
 
 class App extends React.Component {
 
   render() {
     return (
       <MainLayout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/posts" exact component={Posts}/>
-          <Route path='/posts/new' exact component={NewPost}/>
-          <Route path='/posts/:id' exact component={SinglePost}/>
-          <Route path="/contact" exact component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
+        <Suspense fallback={<div>Wczytywanie...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/posts" exact component={Posts}/>
+            <Route path='/posts/new' exact component={NewPost}/>
+            <Route path='/posts/:id' exact component={SinglePost}/>
+            <Route path="/contact" exact component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
       </MainLayout>
     );
   }
