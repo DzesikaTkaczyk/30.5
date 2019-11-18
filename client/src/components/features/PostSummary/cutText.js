d8e913b270801b1879d9b355039c2390db243494
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-11-18 13:37:25 +0100

    Update cutText.js

--- client/src/components/features/PostSummary/cutText.js
+++ client/src/components/features/PostSummary/cutText.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 const cutText = (content, maxLength) => {
-  if(maxLength < 1) return 'Error';
-  if(content.length <= maxLength) return content;
+  if (maxLength < 1) return 'Error';
+  if (content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
 }; 
 
