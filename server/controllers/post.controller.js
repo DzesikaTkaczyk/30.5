d2e437cce3af029384aceaa4e6f5fa6509347798
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-11-22 15:56:11 +0100

    add author

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -4,7 +4,9 @@ const uuid = require('uuid');
 // get all posts
 
 exports.getPosts = async (req, res) => {
+	
 	try {
+		console.log('getposts', await Post.find())
 		res.status(200).json(await Post.find());
 	} catch(err) {
 		res.status(500).json(err);
